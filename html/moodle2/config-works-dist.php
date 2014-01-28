d83538d816e68b2c86563e29b7342ca772682099
Toni Ginard <aginard@xtec.cat>
2014-01-28 13:07:37 +0100

    Improved works.php to check not only connection but data collection

--- html/moodle2/config-works-dist.php
+++ html/moodle2/config-works-dist.php
@@ -8,4 +8,4 @@ $CFG = new stdClass();
 $CFG->dbname = 'xe';
 $CFG->dbuser = 'odissea';
 $CFG->dbpass = 'agora';
-
+$CFG->prefix = 'm2';
--- html/moodle2/works.php
+++ html/moodle2/works.php
@@ -1,13 +1,36 @@
 <?php
-//XTEC ************ FITXER AFEGIT - Fitxer del Nagios per comprovar funciona l'espai web
-//2010.09.01
 
-	require_once('config-works.php');
-	$con = oci_connect($CFG->dbuser, $CFG->dbpass, $CFG->dbname);
-	if ($con){
-		oci_close($con);
-		echo 'Aplicacio:OK';
-	}else{
-		echo 'DB ERROR';
-	}
-?>
+//XTEC ************ FITXER AFEGIT - Fitxer del Nagios per comprovar funciona l'espai web
+//2014.01.28
+
+require_once('config-works.php');
+
+$state = '';
+$con = oci_connect($CFG->dbuser, $CFG->dbpass, $CFG->dbname);
+
+if ($con) {
+    $sql = 'SELECT count(*) FROM ' . $CFG->prefix . 'course WHERE category = 0';
+
+    $stmt = oci_parse($con, $sql);
+
+    if (oci_execute($stmt, OCI_DEFAULT)) {
+        if (oci_fetch($stmt)) {
+            $result = oci_result($stmt, 1);
+            if (is_numeric($result) && $result > 0) {
+                $state = 'Aplicacio:OK';
+            } else {
+                $state = '<br />No s\'ha trobat el registre esperat a la taula ' . $CFG->prefix . 'course de l\'usuari ' . $CFG->dbuser . ' de la instància ' . $CFG->dbname;
+            }
+        } else {
+            $state = '<br />No s\'ha pogut accedir a la taula ' . $CFG->prefix . 'course de l\'usuari ' . $CFG->dbuser . ' de la instància ' . $CFG->dbname;
+        }
+    } else {
+        $state = '<br />No s\'ha pogut executar la consulta de validació a la taula ' . $CFG->prefix . 'course de l\'usuari ' . $CFG->dbuser . ' de la instància ' . $CFG->dbname;
+    }
+} else {
+    $state = '<br />No s\'ha pogut connectar a la inst&agrave;ncia ' . $CFG->dbname . ' (usuari ' . $CFG->dbuser . ')';
+}
+
+oci_close($con);
+
+echo $state;
