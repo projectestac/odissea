a971af360af0c31fb9370811a9949b4f30c24812
pau.ferrer-ocana <pau.ferrer-ocana@upcnet.es>
2014-09-25 14:14:45 +0200

    Added settings.php

---
+++ html/moodle2/settings.php
@@ -0,0 +1,31 @@
+<?php
+
+$dirroot = dirname(dirname(dirname(__FILE__)));
+
+$CFG->altcacheconfigpath = $dirroot.'/html/moodle2/local/agora/muc/';
+
+$CFG->langotherroot = dirname(__FILE__) . '/lang/';
+
+$CFG->directorypermissions = 00777;  // try 02777 on a server in Safe Mode
+
+$CFG->alternateloginurl = '';
+$CFG->mymoodleredirect = 0;
+$CFG->isagora = 0;
+$CFG->iseoi = 0;
+$CFG->isportal = 0;
+$CFG->diskPercent = 0;
+$CFG->disableupdatenotifications = 1;
+$CFG->cachetext = 0; // Disabled to enhace the performance
+
+$CFG->session_handler_class = '\core\session\file';
+$CFG->session_file_save_path = ini_get('session.save_path');
+
+//$CFG->apligestmail = 1;
+//$CFG->apligestlog = 1;
+//$CFG->apligestlogdebug = 1;
+$CFG->apligestlogpath = $CFG->dataroot.'/repository/files/mailsender.log';
+$CFG->apligestaplic = 'ODISSEA';
+
+$CFG->agora_muc_path = $dirroot.'/cache_ins';
+$CFG->cachedir = $CFG->agora_muc_path.'/cache';
+$CFG->localcachedir = $CFG->agora_muc_path.'/localcache';
