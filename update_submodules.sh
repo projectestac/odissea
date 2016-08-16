fb9f793b09cd2e9a5640abd4023b7fef4296b421
Sara Arjona <sarjona@xtec.cat>
2016-08-16 14:25:35 +0200

    Updated configurable_reports version to Moodle 3

--- update_submodules.sh
+++ update_submodules.sh
@@ -5,6 +5,6 @@ source "update_functions.sh"
 #El tercer paràmetre només es posa si el repositori és nostre per poder-hi escriure
 gitcheckout "html/moodle2/admin/tool/odisseagtafsync" "master" "git@github.com:projectestac/moodle-admin_odisseagtafsync.git"
 gitcheckout "html/moodle2/auth/odissea" "master" "git@github.com:projectestac/moodle-auth_odissea.git"
-gitcheckout "html/moodle2/blocks/configurable_reports" "CR_23_STABLE" "https://github.com/jleyva/moodle-block_configurablereports.git"
+gitcheckout "html/moodle2/blocks/configurable_reports" "MOODLE_30_STABLE" "https://github.com/projectestac/moodle-block_configurablereports.git"
 gitcheckout "html/moodle2/testlib" "master" "git@github.com:projectestac/testlib_PHP.git"
 
