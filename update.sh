0d372d745f0e8c380dcfeea9f8add0853463d604
Toni Ginard <toni.ginard@gmail.com>
2020-10-29 13:41:34 +0100

    Added scripts to update application

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source "update_functions.sh"
+
+get_action $1
+
+echo 'Pull inicial'
+git_pull master
+
+pull_submodules
+
+echo "Garbage collecting..."
+git gc
+
+echo "That's all folks!"
