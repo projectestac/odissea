9ee6b0b3500d6976f465be1e5706d9485fc4f6b7
Ignacio Abejaro <ignacio.abejaro@ticxcat.cat>
2020-12-17 11:38:46 +0100

    Changed email bus to FMO

--- html/moodle2/local/agora/mailer/mailsender.class.php
+++ html/moodle2/local/agora/mailer/mailsender.class.php
@@ -37,14 +37,12 @@ class mailsender {
      * Url of the allowed environments
      */
     private static $allowed_environments = array(
-        //'DES' => 'http://integracio.bus.ensenyament.intranet.gencat.cat/event/ServeisComuns/intern/EnviaCorreu/a1/EnviaCorreu',
         'DES' => 'http://xtec-wc.educacio.intranet:8080/esb/slide/ESB_Projects/ESB-EnviaCorreu_ESB-enviaCorreu/INT/ESB-Correu.wsdl',
-        //'INT' => 'http://integracio.bus.ensenyament.intranet.gencat.cat/event/ServeisComuns/intern/EnviaCorreu/a1/EnviaCorreu',
-        'INT' => 'http://xtec-wc.educacio.intranet:8080/esb/slide/ESB_Projects/ESB-EnviaCorreu_ESB-enviaCorreu/INT/ESB-Correu.wsdl',
-        'ACC' => 'http://acc.xtec.cat:8080/event/ServeisComuns/intern/EnviaCorreu/a1/EnviaCorreu',
-        'PRE' => 'http://preproduccio.bus.ensenyament.intranet.gencat.cat/event/ServeisComuns/intern/EnviaCorreu/a1/EnviaCorreu',
-        'PRO' => 'http://aplitic.xtec.cat:8080/event/ServeisComuns/intern/EnviaCorreu/a1/EnviaCorreu',
-        'FRM' => 'http://aplitic.xtec.cat:8080/event/ServeisComuns/intern/EnviaCorreu/a1/EnviaCorreu'
+        'INT' => 'http://integracio.bus.ensenyament.intranet.gencat.cat/event/ServeisComuns/intern/EnviaCorreu/a1/EnviaCorreu?WSDL',
+        'ACC' => 'http://preproduccio.bus.ensenyament.intranet.gencat.cat/event/ServeisComuns/intern/EnviaCorreu/a1/EnviaCorreu/CorreuHttpPort?WSDL',
+        'PRE' => 'http://preproduccio.bus.ensenyament.intranet.gencat.cat/event/ServeisComuns/intern/EnviaCorreu/a1/EnviaCorreu/CorreuHttpPort?WSDL',
+        'PRO' => 'http://bus.ensenyament.intranet.gencat.cat/event/ServeisComuns/intern/EnviaCorreu/a1/EnviaCorreu/CorreuHttpPort?WSDL',
+        'FRM' => 'http://bus.ensenyament.intranet.gencat.cat/event/ServeisComuns/intern/EnviaCorreu/a1/EnviaCorreu/CorreuHttpPort?WSDL'
     );
 
     private static $maxretry = 5;
@@ -110,15 +108,14 @@ class mailsender {
             if (!$this->init_soap()) {
                 throw new Exception('Mailsender init_soap: Cannot load soap, service not avalaible or invalid wsdl');
             }
-        } catch (Throwable $e) {
-
+        } catch (Exception $e) {
             $this->add_log('get_soapclient: '.$e->getMessage(), 'ERROR');
             throw $e;
         }
 
         try {
             $this->test_availability();
-        } catch (Throwable $e) {
+        } catch (Exception $e) {
             throw new Exception('Mailsender: Test availability failed with message "'. $e->getMessage().'"');
         }
         //$this->add_log('Class loaded successfull', 'DEBUG');
@@ -204,7 +201,7 @@ class mailsender {
 
             $this->add_log('send_mail: Send mail SoapFault Exception KO', 'ERROR');
             return false;
-        } catch (Throwable $e) {
+        } catch (Exception $e) {
             // In case of KO, do not try again
             $this->clear_messages();
             $this->add_log('send_mail: Send mail KO', 'ERROR');
@@ -598,76 +595,224 @@ class mailsender {
      * @return string    -> mime type of the given file
      */
     private function get_mime_type($fn) {
-        
-        $mime_types = array(
-            
-            'txt' => 'text/plain',
-            'htm' => 'text/html',
-            'html' => 'text/html',
-            'php' => 'text/html',
-            'css' => 'text/css',
-            'js' => 'application/javascript',
-            'json' => 'application/json',
-            'xml' => 'application/xml',
-            'swf' => 'application/x-shockwave-flash',
-            'flv' => 'video/x-flv',
-            
-            // images
-            'png' => 'image/png',
-            'jpe' => 'image/jpeg',
-            'jpeg' => 'image/jpeg',
-            'jpg' => 'image/jpeg',
-            'gif' => 'image/gif',
-            'bmp' => 'image/bmp',
-            'ico' => 'image/vnd.microsoft.icon',
-            'tiff' => 'image/tiff',
-            'tif' => 'image/tiff',
-            'svg' => 'image/svg+xml',
-            'svgz' => 'image/svg+xml',
-            
-            // archives
-            'zip' => 'application/zip',
-            'rar' => 'application/x-rar-compressed',
-            'exe' => 'application/x-msdownload',
-            'msi' => 'application/x-msdownload',
-            'cab' => 'application/vnd.ms-cab-compressed',
-            
-            // audio/video
-            'mp3' => 'audio/mpeg',
-            'qt' => 'video/quicktime',
-            'mov' => 'video/quicktime',
-            
-            // adobe
-            'pdf' => 'application/pdf',
-            'psd' => 'image/vnd.adobe.photoshop',
-            'ai' => 'application/postscript',
-            'eps' => 'application/postscript',
-            'ps' => 'application/postscript',
-            
-            // ms office
-            'doc' => 'application/msword',
-            'rtf' => 'application/rtf',
-            'xls' => 'application/vnd.ms-excel',
-            'ppt' => 'application/vnd.ms-powerpoint',
-            
-            // open office
-            'odt' => 'application/vnd.oasis.opendocument.text',
-            'ods' => 'application/vnd.oasis.opendocument.spreadsheet',
-        );
-        
-        $ext = strtolower(array_pop(explode('.',$fn)));
-        if (array_key_exists($ext, $mime_types)) {
-            return $mime_types[$ext];
-        }
-        elseif (function_exists('finfo_open')) {
-            $finfo = finfo_open(FILEINFO_MIME);
-            $mimetype = finfo_file($finfo, $fn);
-            finfo_close($finfo);
-            return $mimetype;
-        }
-        else {
-            return 'application/octet-stream';
-        }
+
+        static $mime_magic_data;
+
+      #-- fallback
+      $type = false;
+
+      #-- read in first 3K of given file
+      if (is_dir($fn)) {
+         return("httpd/unix-directory");
+      }
+      elseif (is_resource($fn) || ($fn = @fopen($fn, "rb"))) {
+         $bin = fread($fn, $maxlen=3072);
+         fclose($fn);
+      }
+      elseif (!file_exists($fn)) {
+         return false;
+      }
+      else {
+         return("application/octet-stream");   // give up
+      }
+
+      #-- use PECL::fileinfo when available
+      if (function_exists("finfo_buffer")) {
+         if (!isset($mime_magic_data)) {
+            $mime_magic_data = finfo_open(MAGIC_MIME);
+         }
+         $type = finfo_buffer($bin);
+         return($type);
+      }
+
+      #-- read in magic data, when called for the very first time
+      if (!isset($mime_content_type)) {
+         if (file_exists($fn = ini_get("mime_magic.magicfile")) || file_exists($fn = "./lib/magic.mime") || file_exists($fn = "../lib/magic.mime"))
+         {
+            $mime_magic_data = array();
+
+            #-- read in file
+            $f = fopen($fn, "r");
+            $fd = fread($f, 1<<20);
+            fclose($f);
+            $fd = str_replace("       ", "\t", $fd);
+
+            #-- look at each entry
+            foreach (explode("\n", $fd) as $line) {
+
+               #-- skip empty lines
+               if (!strlen($line) or ($line[0] == "#") or ($line[0] == "\n")) {
+                  continue;
+               }
+
+               #-- break into four fields at tabs
+               $l = preg_split("/\t+/", $line);
+               @list($pos, $typestr, $magic, $ct) = $l;
+#print_r($l);
+
+               #-- ignore >continuing lines
+               if ($pos[0] == ">") {
+                  continue;
+               }
+               #-- real mime type string?
+               $ct = strtok($ct, " ");
+               if (!strpos($ct, "/")) {
+                  continue;
+               }
+
+               #-- mask given?
+               $mask = 0;
+               if (strpos($typestr, "&")) {
+                  $typestr = strtok($typestr, "&");
+                  $mask = strtok(" ");
+                  if ($mask[0] == "0") {
+                     $mask = ($mask[1] == "x") ? hexdec(substr($mask, 2)) : octdec($mask);
+                  }
+                  else {
+                     $mask = (int)$mask;
+                  }
+               }
+
+               #-- strip prefixes
+               if ($magic[0] == "=") {
+                  $magic = substr($magic, 1);
+               }
+
+               #-- convert type
+               if ($typestr == "string") {
+                  $magic = stripcslashes($magic);
+                  $len = strlen($magic);
+                  if ($mask) {
+                     continue;
+                  }
+               }
+               #-- numeric values
+               else {
+
+                  if ((ord($magic[0]) < 48) or (ord($magic[0]) > 57)) {
+#echo "\nmagicnumspec=$line\n";
+#var_dump($l);
+                     continue;  #-- skip specials like  >, x, <, ^, &
+                  }
+
+                  #-- convert string representation into int
+                  if ((strlen($magic) >= 4) && ($magic[1] == "x")) {
+                     $magic = hexdec(substr($magic, 2));
+                  }
+                  elseif ($magic[0]) {
+                     $magic = octdec($magic);
+                  }
+                  else {
+                     $magic = (int) $magic;
+                     if (!$magic) { continue; }   // zero is not a good magic value anyhow
+                  }
+
+                  #-- different types
+                  switch ($typestr) {
+
+                     case "byte":
+                        $len = 1;
+                        break;
+
+                     case "beshort":
+                        $magic = ($magic >> 8) | (($magic & 0xFF) << 8);
+                     case "leshort":
+                     case "short":
+                        $len = 2;
+                        break;
+
+                     case "belong":
+                        $magic = (($magic >> 24) & 0xFF)
+                               | (($magic >> 8) & 0xFF00)
+                               | (($magic & 0xFF00) << 8)
+                               | (($magic & 0xFF) << 24);
+                     case "lelong":
+                     case "long":
+                        $len = 4;
+                        break;
+
+                     default:
+                        // date, ldate, ledate, leldate, beldate, lebelbe...
+                        continue;
+                  }
+               }
+
+               #-- add to list
+               $mime_magic_data[] = array($pos, $len, $mask, $magic, trim($ct));
+            }
+         }
+//echo "mime_magic_data = "; print_r($mime_magic_data);
+      }
+
+
+      #-- compare against each entry from the mime magic database
+      foreach ($mime_magic_data as $def) {
+
+         #-- entries are organized as follows
+         list($pos, $len, $mask, $magic, $ct) = $def;
+
+         #-- ignored entries (we only read first 3K of file for opt. speed)
+         if ($pos >= $maxlen) {
+            continue;
+         }
+
+         $slice = substr($bin, $pos, $len);
+         #-- integer comparison value
+         if ($mask) {
+            $value = hexdec(bin2hex($slice));
+            if (($value & $mask) == $magic) {
+               $type = $ct;
+               break;
+            }
+         }
+         #-- string comparison
+         else {
+            if ($slice == $magic) {
+               $type = $ct;
+               break;
+            }
+         }
+      }// foreach
+
+      #-- built-in defaults
+      if (!$type) {
+
+         #-- some form of xml
+         if (strpos($bin, "<"."?xml ") !== false) {
+            return("text/xml");
+         }
+         #-- html
+         elseif ((strpos($bin, "<html>") !== false) || (strpos($bin, "<HTML>") !== false)
+         || strpos($bin, "<title>") || strpos($bin, "<TITLE>")
+         || (strpos($bin, "<!--") !== false) || (strpos($bin, "<!DOCTYPE HTML ") !== false)) {
+            $type = "text/html";
+         }
+         #-- mail msg
+         elseif ((strpos($bin, "\nReceived: ") !== false) || strpos($bin, "\nSubject: ")
+         || strpos($bin, "\nCc: ") || strpos($bin, "\nDate: ")) {
+            $type = "message/rfc822";
+         }
+         #-- php scripts
+         elseif (strpos($bin, "<"."?php") !== false) {
+            return("application/x-httpd-php");
+         }
+         #-- plain text, C source or so
+         elseif (strpos($bin, "function ") || strpos($bin, " and ")
+         || strpos($bin, " the ") || strpos($bin, "The ")
+         || (strpos($bin, "/*") !== false) || strpos($bin, "#include ")) {
+            return("text/plain");
+         }
+
+         #-- final fallback
+         else {
+            $type = "application/octet-stream";
+         }
+      }
+
+
+
+      #-- done
+      return $type;
     }
 
     ////////////////////////////////////////////////////////
@@ -688,7 +833,7 @@ class mailsender {
 
         try {
             return log4p::instance(true, $path, $debug);
-        } catch (Throwable $e) {
+        } catch (Exception $e) {
             debugging('ERROR: Cannot initialize apligestlogger, there won\'t be any log.');
             debugging($e->getMessage());
         }
