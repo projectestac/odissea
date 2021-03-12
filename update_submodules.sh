8ed0db0dc41d2be3b64d9c24f53e26b394fc9950
Toni Ginard <toni.ginard@gmail.com>
2021-03-12 11:54:41 +0100

    Added redislock to script to update submodules

--- update_submodules.sh
+++ update_submodules.sh
@@ -3,5 +3,6 @@
 source "update_functions.sh"
 
 gitcheckout "html/admin/tool/odisseagtafsync" "master" "git@github.com:projectestac/moodle-admin_odisseagtafsync.git"
-gitcheckout "html/blocks/configurable_reports" "MOODLE_36_STABLE" "https://github.com/jleyva/moodle-block_configurablereports.git"
 gitcheckout "html/auth/saml2" "master" "https://github.com/projectestac/moodle-auth_saml2.git"
+gitcheckout "html/blocks/configurable_reports" "MOODLE_36_STABLE" "https://github.com/jleyva/moodle-block_configurablereports.git"
+gitcheckout "html/local/redislock" "MOODLE_38_STABLE" "https://github.com/open-lms-open-source/moodle-local_redislock.git"
