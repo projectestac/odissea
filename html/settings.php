d1f4bbee89adacadc2991662202eadb33e96d854
Toni Ginard <toni.ginard@gmail.com>
2021-03-12 11:50:34 +0100

    Added parameters to store sessions in redis

--- html/settings.php
+++ html/settings.php
@@ -23,9 +23,9 @@ $CFG->directorypermissions = 0777;
 $CFG->disableupdatenotifications = 1;
 $CFG->slasharguments = true;
 
-$CFG->session_handler_class = '\core\session\database';
-$CFG->session_database_acquire_lock_timeout = 120;
-$CFG->sessioncookie = $CFG->dbuser;
+// $CFG->session_handler_class = '\core\session\database';
+// $CFG->session_database_acquire_lock_timeout = 120;
+$CFG->sessioncookie = 'odissea';
 
 $CFG->isagora = false;
 $CFG->iseoi = false;
@@ -48,8 +48,18 @@ $CFG->mailheader = '[Odissea]';
 // This variable must be defined
 $CFG->apligestenv = '';
 
-// Change locking from NFS to DB
-$CFG->lock_factory = "\\core\\lock\\db_record_lock_factory";
+// Change locking from NFS to redis
+$CFG->lock_factory = '\\local_redislock\\lock\\redis_lock_factory';
+
+// Configure redis for sessions
+$CFG->session_handler_class = '\core\session\redis';
+$CFG->session_redis_database = 0;  // Optional, default is db 0.
+$CFG->session_redis_auth = ''; // Optional, default is don't set one.
+$CFG->session_redis_prefix = 'odi_sess_'; // Optional, default is don't set one.
+$CFG->session_redis_acquire_lock_timeout = 120;
+$CFG->session_redis_acquire_lock_retry = 100; // Optional, default is 100ms (from 3.9)
+$CFG->session_redis_lock_expire = 7200;
+$CFG->session_redis_serializer_use_igbinary = false; // Optional, default is PHP builtin serializer.
 
 $CFG->forced_plugin_settings = [
     'block_configurable_reports' => [
