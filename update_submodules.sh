e397e7a64052cc6d0291b8b13580445be3a09cc1
Toni Ginard <toni.ginard@gmail.com>
2024-10-08 14:37:51 +0200

    update_submodules: Added submodule to update script

--- update_submodules.sh
+++ update_submodules.sh
@@ -2,6 +2,7 @@
 
 source "update_functions.sh"
 
-gitcheckout "html/admin/tool/odisseagtafsync" "master" "git@github.com:projectestac/moodle-admin_odisseagtafsync.git"
-gitcheckout "html/blocks/configurable_reports" "MOODLE_36_STABLE" "https://github.com/jleyva/moodle-block_configurablereports.git"
+gitcheckout "html/admin/tool/odisseagtafsync" "master" "https://github.com/projectestac/moodle-admin_odisseagtafsync.git"
 gitcheckout "html/auth/saml2" "master" "https://github.com/projectestac/moodle-auth_saml2.git"
+gitcheckout "html/blocks/configurable_reports" "MOODLE_36_STABLE" "https://github.com/jleyva/moodle-block_configurablereports.git"
+gitcheckout "html/course/format/tiles" "moodle41b" "https://bitbucket.org/dw8/moodle-format_tiles.git"
