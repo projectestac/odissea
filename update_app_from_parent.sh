91f677fbc211e20558d073bbdc26fafbf7c4834d
Toni Ginard <toni.ginard@gmail.com>
2025-07-09 17:37:12 +0200

    update_app_from_parent.sh: Minor improvements

--- update_app_from_parent.sh
+++ update_app_from_parent.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
 # Call example: ./update_app_from_parent.sh
+# version=25.10.28
 
-version=20.11.03
-
-pushd /tmp
+pushd /tmp || return
 
 git clone https://github.com/projectestac/agora_moodle2.git
 git clone https://github.com/projectestac/odissea.git
 
-pushd agora_moodle2
+pushd agora_moodle2 || return
 git submodule update --init --recursive
 find . -name '\.git*' -exec rm -rf {} \; 2>/dev/null
-popd
+popd || return
 
-data=`date +"%Y%m%d"`
+data=$(date +"%Y%m%d")
 
-pushd odissea
-git checkout -b UPSTREAM origin/UPSTREAM
+pushd odissea || return
+git checkout -b UPSTREAM-4.5 origin/UPSTREAM-4.5
 rm -rf html/
 mv ../agora_moodle2 html
 git add -A
 git commit -m 'Updated from agora_moodle2 repository ('$data')'
-git push origin UPSTREAM
-popd
+git push origin UPSTREAM-4.5
+popd || return
 
-popd
+popd || return
 
 printf "\nRemaining work: rebase master using UPSTREAM. Conflicts may arise.\n"
